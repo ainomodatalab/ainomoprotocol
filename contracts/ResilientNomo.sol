424eae00ba410009f90239cb3a4967c979fa059f
AINOMO <165270740+ainomodatalab@users.noreply.github.com>
2024-04-11 06:13:48 -0700

    Update ResilientNomo.sol

--- contracts/ResilientNomo.sol
+++ contracts/ResilientNomo.sol
@@ -1,4 +1,4 @@
-4444444444pragma solidity 0.8.13;
+44444444444pragma solidity 0.8.13;
 
 import "../contracts-upgradeable/security/PausableUpgradeable.sol";
 import "./interfaces/VBep20Interface.sol";
